821bef4a5d4711a84a85c93b1fa4a13180436f98
Thura Aung <149793651+MadThura@users.noreply.github.com>
2026-07-10 18:53:29 +0630

    fix: resolve accidental conflict markers merged in dev (#8)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
-        
+        $middleware->alias([
+            'guest' => GuestMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
