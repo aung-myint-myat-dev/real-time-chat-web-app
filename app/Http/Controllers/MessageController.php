d891a3e1720e5d6314b618c4c50c6402db0436f0
MadThura <madthura02@gmail.com>
2026-07-18 18:43:41 +0630

    refactor:store message function using SendMessageAction

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,27 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\Message\MessageSent;
+use App\Actions\Message\SendMessageAction;
 use App\Http\Requests\Message\StoreMessageRequest;
-use App\Models\Message;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    public function store(StoreMessageRequest $request)
-    {
-
-        $validated = $request->validated();
-
-        $message = Message::create([
-            'conversation_id' => $validated['conversation_id'],
-            'user_id' => $validated['user_id'],
-            'body' => $validated['body'],
-            'reply_message_id' => $validated['reply_message_id'],
-            'type' => $validated['type']
-        ]);
-
-        broadcast(new MessageSent($message))->toOthers();
+    public function store(
+        StoreMessageRequest $request,
+        SendMessageAction $action
+    ) {
+        $action->execute($request->validated());
 
         return back();
     }
