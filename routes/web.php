ae47e87cfc5ad5bc55393df365bd1b8c9d1729df
MadThura <madthura02@gmail.com>
2026-08-05 19:22:31 +0630

    refactor: web.php

--- routes/web.php
+++ routes/web.php
@@ -18,29 +18,32 @@ Route::middleware(['guest'])->get('/', function () {
 Route::middleware(['auth'])
     ->group(function () {
 
+        // Chats
         Route::prefix('/chats')
             ->name('chats.')
             ->controller(ChatController::class)
             ->group(function () {
-                Route::get('/', 'index')->name('chatboard');
+                Route::get('/', 'index')->name('index');
                 Route::post('/', 'store')->name('store');
                 Route::get('/{conversation}', 'show')->name('show');
                 Route::post('/{conversation}/read', 'markAsRead')->name('read');
             });
 
+        // Messages
         Route::get('/chats/{conversation}/messages', [MessageController::class, 'index'])->name('message.index');
         Route::post('/messages', [MessageController::class, 'store'])->name('message.store');
         Route::post('/messages/{message}/mark-as-read', [MessageController::class, 'markAsRead'])->name('message.read');
 
-        Route::get('/users/search', [UserSearchController::class, 'index']);
+        Route::get('/users/search', [UserSearchController::class, 'index'])->name('users.search');;
 
+        // Profile
         Route::prefix('/profile')->name('profile.')->controller(ProfileController::class)->group(function () {
             Route::get('/', 'index')->name('index');
             Route::put('/', 'update')->name('update');
             Route::delete('/', 'delete')->name('delete');
             Route::put('/avatar', 'uploadProfileImage')->name('avatar.upload');
             Route::delete('/avatar', 'deleteProfileImage')->name('avatar.delete');
-            Route::get('/{id}', 'show')->name('show');
+            Route::get('/{user}', 'show')->name('show');
         });
 
         Route::post('/users/update-last-seen-at',  [UserController::class, 'updateLastSeenAt']);
