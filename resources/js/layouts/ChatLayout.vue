6ba8247ae3e65298ea226c2d810e5959c8a57014
MadThura <madthura02@gmail.com>
2026-08-09 13:52:50 +0630

    fix:online status

--- resources/js/layouts/ChatLayout.vue
+++ resources/js/layouts/ChatLayout.vue
@@ -85,68 +85,61 @@ provide("BackToListsHandaler", { handleBackToLists });
 provide("HaldleSelectedChatId", { handleSelectedChatId });
 provide("handleSelectedSearchedUser", { handleSelectedSearchedUser });
 
-// onMounted(() => {
-//     if (typeof window !== 'undefined') {
-//         window.addEventListener("popstate", handlePopState);
-//     }
-//     Echo.join("online")
-//         .here((users) => {
-//             users.forEach((user) => {
-//                 onlineUsersStore.addOnlineUserId(user.id);
-//             });
-//         })
-//         .joining((user) => {
-//             onlineUsersStore.addOnlineUserId(user.id);
-//         })
-//         .leaving((user) => {
-//             onlineUsersStore.removeOnlineUserId(user.id);
-//         })
-//         .error((e) => {
-//             console.log(e);
-//         });
-//     Echo.private(`users.${page.props.auth.user.id}`)
-//         .listen(".message.sent", (e) => {
-//             if (e.user_id !== page.props.auth.user.id && selectedChatId.value !== e.conversation_id) {
-//                 playSound();
-//                 notificationStore.add(e);
-//             }
-//         });
-
-//     conversationStore.conversations.forEach((conversation) => {
-//         Echo.private(`chats.${conversation.id}`)
-//             .listen(".message.sent", (e) => {
-//                 // Ignore messages sent by yourself
-//                 if (e.user_id === page.props.auth.user.id) return;
-
-//                 // Ignore the currently opened conversation
-//                 if (e.conversation_id === selectedChatId.value) return;
-
-//                 conversationStore.addUnreadMessage(
-//                     e.conversation_id,
-//                     e.id
-//                 );
-//             });
-//     });
-
-// });
-
-// onUnmounted(() => {
-//     Echo.leave(`users.${page.props.auth.user.id}`);
-
-//     conversationStore.conversations.forEach((conversation) => {
-//         Echo.leave(`chats.${conversation.id}`);
-//     });
-
-//     if (typeof window !== 'undefined') {
-//         window.removeEventListener("popstate", handlePopState);
-//     }
-// });
 onMounted(() => {
-    console.log("ChatLayout mounted");
+    if (typeof window !== 'undefined') {
+        window.addEventListener("popstate", handlePopState);
+    }
+    Echo.join("online")
+        .here((users) => {
+            users.forEach((user) => {
+                onlineUsersStore.addOnlineUserId(user.id);
+            });
+        })
+        .joining((user) => {
+            onlineUsersStore.addOnlineUserId(user.id);
+        })
+        .leaving((user) => {
+            onlineUsersStore.removeOnlineUserId(user.id);
+        })
+        .error((e) => {
+            console.log(e);
+        });
+    Echo.private(`users.${page.props.auth.user.id}`)
+        .listen(".message.sent", (e) => {
+            if (e.user_id !== page.props.auth.user.id && selectedChatId.value !== e.conversation_id) {
+                playSound();
+                notificationStore.add(e);
+            }
+        });
+
+    conversationStore.conversations.forEach((conversation) => {
+        Echo.private(`chats.${conversation.id}`)
+            .listen(".message.sent", (e) => {
+                // Ignore messages sent by yourself
+                if (e.user_id === page.props.auth.user.id) return;
+
+                // Ignore the currently opened conversation
+                if (e.conversation_id === selectedChatId.value) return;
+
+                conversationStore.addUnreadMessage(
+                    e.conversation_id,
+                    e.id
+                );
+            });
+    });
+
 });
 
 onUnmounted(() => {
-    console.log("ChatLayout unmounted");
+    Echo.leave(`users.${page.props.auth.user.id}`);
+
+    conversationStore.conversations.forEach((conversation) => {
+        Echo.leave(`chats.${conversation.id}`);
+    });
+
+    if (typeof window !== 'undefined') {
+        window.removeEventListener("popstate", handlePopState);
+    }
 });
 </script>
 
